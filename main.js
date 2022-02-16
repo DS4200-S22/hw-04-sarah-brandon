f8b76eeab49c974ca48ce47d82886e98e5f1bd4b
Brandon Onyejekwe <brandononyejekwe@Brandons-MacBook-Pro.local>
2022-02-16 01:14:12 -0500

    Added function

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
-
-function f1() {
-	
+function UpdateCircleClick() {
+	console.log("Button was clicked");
+	let old_num = document.getElementByID("num_button_clicks");
+	let new_num = old_num + 1;
+	old_num.innerHTML = new_num;
 }
