474d232fd4aab2b8d0a8226b39fd4b14a9be2d1f
Sarah Costa <sarahcosta@Sarahs-MacBook-Pro.local>
2022-02-17 10:08:05 -0500

    disappear circle

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
+// let num = 0;
+// num = addOne(num);
+// console.log(num)
+
+
+// function addOne(num) {
+// 	num = num + 1
+// 	return num
+// }
 
 function UpdateCircleClick() {
+
 	console.log("Button was clicked");
-	let old_num = document.getElementByID("num_button_clicks");
-	let new_num = old_num + 1;
-	old_num.innerHTML = new_num;
+
+	// var button = document.getElementById("btn");
+	// var text1 = document.getElementById("text1");
+
+	// button.addEventListener('click', handleEvent: function (event) {
+ //  		funcOutput.textContent = 'Element clicked through function!';
+	// });
+
+	// let newNum = parseInt(text1) + 1
+	// text1.innerHTML = newNum;
+}
+
+function disappearCircle() {
+	var circle = document.getElementById("circle3");
+	circle.style.display = "none";
 }
